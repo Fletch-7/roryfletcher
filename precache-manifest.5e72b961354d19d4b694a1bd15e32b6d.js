bf0a310d56e491d92a2eab7ca45b4c2f26d27d06
Fletch-7 <r.a.m.fletcher7@gmail.com>
2020-03-30 14:37:33 +0100

    Updates

---
+++ precache-manifest.5e72b961354d19d4b694a1bd15e32b6d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a121086bb4def764631a130c5972a2",
+    "url": "/roryfletcher/index.html"
+  },
+  {
+    "revision": "00bdd068a0a215e162bd",
+    "url": "/roryfletcher/static/css/2.49233dca.chunk.css"
+  },
+  {
+    "revision": "eb8e3de0b4d3a1c1c998",
+    "url": "/roryfletcher/static/css/main.bc203878.chunk.css"
+  },
+  {
+    "revision": "00bdd068a0a215e162bd",
+    "url": "/roryfletcher/static/js/2.73bdd277.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roryfletcher/static/js/2.73bdd277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8e3de0b4d3a1c1c998",
+    "url": "/roryfletcher/static/js/main.8c663c8d.chunk.js"
+  },
+  {
+    "revision": "21361bc21a6499036e4a",
+    "url": "/roryfletcher/static/js/runtime-main.036dd4a4.js"
+  },
+  {
+    "revision": "ef1c2c1ba346cc9321496427961400ae",
+    "url": "/roryfletcher/static/media/Geocachr.ef1c2c1b.png"
+  },
+  {
+    "revision": "486b18a5465794012add136dac838f67",
+    "url": "/roryfletcher/static/media/Hero_API.486b18a5.png"
+  },
+  {
+    "revision": "738d58615aef9fd90110665f277ffd78",
+    "url": "/roryfletcher/static/media/Pensive.738d5861.png"
+  },
+  {
+    "revision": "1a2e7ccea2c88192f2120e5a535f252c",
+    "url": "/roryfletcher/static/media/Snake_Game.1a2e7cce.png"
+  }
+]);
